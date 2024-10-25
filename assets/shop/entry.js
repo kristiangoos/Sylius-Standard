55b0a61ac06e9de250ea8dbb71bc06e839bf7c0b
Kristian Kostov <kristian.kostov@gmail.com>
2024-10-25 10:50:44 +0300

    Override quantity field values. JS Check if 70

--- assets/shop/entry.js
+++ assets/shop/entry.js
@@ -1,1 +1,10 @@
-// In this file you can import assets like images or stylesheets
+// Check if selected quantity is 70
+
+const matchVal = 70;
+const quantity = document.getElementById('sylius_add_to_cart_cartItem_quantity');
+
+if (quantity) {
+  quantity.addEventListener('change', (e) => {
+    (matchVal === parseInt(e.target.value, 10)) ? alert(`${e.target.value} is great choice`) : null;
+  });
+}
